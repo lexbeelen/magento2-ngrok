b627fd46b4b73a7543131d58244d73a4f4ea1036
Lex Beelen <lex@lexbeelen.nl>
2017-02-23 15:04:45 +0100

    Update version 1.0.2. Checks if you’re in developer mode

--- Plugin/Magento/Store/Model/Store.php
+++ Plugin/Magento/Store/Model/Store.php
@@ -11,6 +11,21 @@ class Store
     const NGROK_URL = 'ngrok.io';
 
     /**
+     * @var \Magento\Deploy\Model\Mode
+     */
+    protected $state;
+
+    /**
+     * Store constructor.
+     * @param \Magento\Framework\App\State $state
+     */
+    public function __construct(
+        \Magento\Framework\App\State $state
+    ) {
+        $this->state = $state;
+    }
+
+    /**
      * Get Protocol
      *
      * @return string
@@ -23,12 +38,27 @@ class Store
     }
 
     /**
+     * Is Developer Mode
+     *
+     * @return bool
+     */
+    protected function isDeveloperMode()
+    {
+        return $this->state->getMode() === \Magento\Framework\App\State::MODE_DEVELOPER;
+    }
+
+    /**
      * @param \Magento\Store\Model\Store $subject
      * @param $result
      * @return string
      */
     public function afterGetBaseUrl(\Magento\Store\Model\Store $subject, $result)
     {
+        if(!$this->isDeveloperMode())
+        {
+            return $result;
+        }
+
         if (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], self::NGROK_URL) && in_array($result, array($subject->getConfig($subject::XML_PATH_SECURE_BASE_URL), $subject->getConfig($subject::XML_PATH_UNSECURE_BASE_URL))))
         {
             return $this->getProtocol() . $_SERVER['HTTP_HOST'] . DIRECTORY_SEPARATOR;
@@ -37,3 +67,4 @@ class Store
         return $result;
     }
 }
+
